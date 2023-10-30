8da8fba35f0fdb0a24dea9ce834de04318a488ff
Ксения <ksenia.lobaeva@gmail.com>
2023-10-30 22:01:13 +0300

    1 дз

---
+++ src/main/java/HomeWork_1/ExtraTask/Square.java
@@ -0,0 +1,11 @@
+package HomeWork_1.ExtraTask;
+
+public class Square {
+    public static void main(String[] args) {
+        int sideSquare = 5;
+        int areaSquare = sideSquare * sideSquare;
+
+        System.out.println("Сторона квадрата = " + sideSquare);
+        System.out.println("Площадь квадрата = " + areaSquare );
+    }
+}
