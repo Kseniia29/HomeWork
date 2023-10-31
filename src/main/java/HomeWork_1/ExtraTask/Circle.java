dc94d4601f557d6b8d74b9d4aa76496b98b340ac
Ксения <ksenia.lobaeva@gmail.com>
2023-10-31 15:27:50 +0300

    1 дз

---
+++ src/main/java/HomeWork_1/ExtraTask/Circle.java
@@ -0,0 +1,15 @@
+package HomeWork_1.ExtraTask;
+
+import static java.lang.System.*;
+
+public class Circle {
+    public static void main(String[] args) {
+        double pi = 3.14;
+        int radius = 5;
+        double areaCircle = pi * radius * radius;
+
+        System.out.println(" Постоянная величина Пи = " + pi);
+        System.out.println(" Радиус круга = " + radius);
+        System.out.println(" Площадь круга = " + areaCircle);
+    }
+}
