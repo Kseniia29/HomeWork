ce78c8257e4d6f12c779ad5e70324409f1330128
Ксения <ksenia.lobaeva@gmail.com>
2023-10-31 17:23:26 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/HomeWork_1/ExtraTask/Triangle.java
@@ -0,0 +1,17 @@
+package HomeWork_1.ExtraTask;
+
+public class Triangle {
+
+    public static void main(String[] args) {
+        int side1 = 5;
+        int side2 = 5;
+        int side3 = 5;
+        double s = (side1 + side2 +side3)/2;
+        double areaTriangle = (int) Math.sqrt(s*((s-side1)*(s-side2)*s-side3));
+
+        System.out.println("Сторона 1 = " + side1);
+        System.out.println("Сторона 2 = " + side2);
+        System.out.println("Сторона 3 = " + side3);
+        System.out.println("Площадь треугольника = " + areaTriangle);
+    }
+}
